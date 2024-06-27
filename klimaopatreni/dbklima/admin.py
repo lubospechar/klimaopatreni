76f27274f389e316347487b05145e9c5b1d2f2fb
Lubos Pechar <lubos.pechar@prirodou.cz>
2024-06-27 13:57:01 +0200

    podskupina

--- klimaopatreni/dbklima/admin.py
+++ klimaopatreni/dbklima/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from dbklima.models import Localization, Group
+from dbklima.models import Localization, Group, SubGroup
 
 class GroupInline(admin.TabularInline):
     model = Group
-    extra = 2  # Kolik prázdných řádků pro nový záznam se zobrazí
+    extra = 2
+
+class SubGroupInline(admin.TabularInline):
+    model = SubGroup
+    extra = 2
+    fields = ('subgroup_name', 'code', 'abstract', 'description')
 
 @admin.register(Localization)
 class LocalizationAdmin(admin.ModelAdmin):
@@ -15,3 +20,19 @@ class LocalizationAdmin(admin.ModelAdmin):
 class GroupAdmin(admin.ModelAdmin):
     list_display = ('group_name', 'localization',)
     list_filter = ('localization', )
+    inlines = [SubGroupInline]
+
+@admin.register(SubGroup)
+class SubGroupAdmin(admin.ModelAdmin):
+    list_display = ('subgroup_name', 'code', 'group', 'abstract')
+    search_fields = ('subgroup_name', 'code', 'group__group_name')
+    list_filter = ('group', 'group__localization')
+    fieldsets = (
+        (None, {
+            'fields': ('group', 'subgroup_name', 'code')
+        }),
+        ('Popis opatření', {
+            'fields': ('abstract', 'description')
+        }),
+    )
+    ordering = ('group', 'subgroup_name')
--- klimaopatreni/dbklima/models.py
+++ klimaopatreni/dbklima/models.py
@@ -23,4 +23,19 @@ class Group(models.Model):
         verbose_name_plural = "Skupiny"
 
 
+class SubGroup(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, verbose_name="Skupina")
+    subgroup_name = models.CharField(max_length=100, verbose_name="Pojmenování podskupiny", unique=True)
+    code = models.CharField(max_length=10, verbose_name="Kód opatření", unique=True)
+    abstract = models.CharField(max_length=255, verbose_name="Shrnutí opatření")
+    description = models.TextField(verbose_name="Podrobný popis opatření", blank=False, null=False,)
+
+    def __str__(self):
+        return f'{self.group}: {self.subgroup_name, self.code}'
+
+    class Meta:
+        verbose_name = "Podskupina"
+        verbose_name_plural = "Podskupiny"
+
+
 

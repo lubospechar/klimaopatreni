5e6ced6fdf35b1dd2da2a07e5e9a3729b7b77195
Lubos Pechar <lubos.pechar@prirodou.cz>
2024-06-27 14:41:57 +0200

    pravidla pro unikatnost v tabulkach Group A SubGroup

--- klimaopatreni/dbklima/models.py
+++ klimaopatreni/dbklima/models.py
@@ -25,7 +25,7 @@ class Group(models.Model):
 
 class SubGroup(models.Model):
     group = models.ForeignKey(Group, on_delete=models.CASCADE, verbose_name="Skupina")
-    subgroup_name = models.CharField(max_length=100, verbose_name="Pojmenování podskupiny", unique=True)
+    subgroup_name = models.CharField(max_length=100, verbose_name="Pojmenování podskupiny")
     code = models.CharField(max_length=10, verbose_name="Kód opatření", unique=True)
     abstract = models.CharField(max_length=255, verbose_name="Shrnutí opatření")
     description = models.TextField(verbose_name="Podrobný popis opatření", blank=False, null=False,)
@@ -34,6 +34,7 @@ class SubGroup(models.Model):
         return f'{self.group}: {self.subgroup_name, self.code}'
 
     class Meta:
+        unique_together = ('group', 'subgroup_name')
         verbose_name = "Podskupina"
         verbose_name_plural = "Podskupiny"
 

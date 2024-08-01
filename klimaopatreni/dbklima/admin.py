d967f54c9b132c0617c9dba50ee4b1ec679d5b97
Luboš Pechar <lubos.pechar@prirodou.cz>
2024-08-01 23:27:06 +0200

    black

--- klimaopatreni/dbklima/admin.py
+++ klimaopatreni/dbklima/admin.py
@@ -1,70 +1,68 @@
 from django.contrib import admin
 from .models import Localization, Group, SubGroup, Advantage, Disadvantage
 
+
 class GroupInline(admin.TabularInline):
     model = Group
     extra = 2
 
+
 class SubGroupInline(admin.TabularInline):
     model = SubGroup
     extra = 2
-    fields = ('subgroup_name', 'code', 'abstract', 'description')
+    fields = ("subgroup_name", "code", "abstract", "description")
+
 
 @admin.register(Localization)
 class LocalizationAdmin(admin.ModelAdmin):
-    list_display = ('localization_name',)
+    list_display = ("localization_name",)
     inlines = [GroupInline]
+
 
 @admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
-    list_display = ('group_name', 'localization',)
-    list_filter = ('localization', )
+    list_display = (
+        "group_name",
+        "localization",
+    )
+    list_filter = ("localization",)
     inlines = [SubGroupInline]
+
 
 @admin.register(SubGroup)
 class SubGroupAdmin(admin.ModelAdmin):
-    list_display = ('subgroup_name', 'code', 'group', 'abstract')
-    search_fields = ('subgroup_name', 'code', 'group__group_name')
-    list_filter = ('group', 'group__localization')
+    list_display = ("subgroup_name", "code", "group", "abstract")
+    search_fields = ("subgroup_name", "code", "group__group_name")
+    list_filter = ("group", "group__localization")
     fieldsets = (
-        (None, {
-            'fields': ('group', 'subgroup_name', 'code')
-        }),
-        ('Popis opatření', {
-            'fields': ('abstract', 'description')
-        }),
-        ('Výhody a Nevýhody', {
-            'fields': ('advantages', 'disadvantages')
-        }),
+        (None, {"fields": ("group", "subgroup_name", "code")}),
+        ("Popis opatření", {"fields": ("abstract", "description")}),
+        ("Výhody a Nevýhody", {"fields": ("advantages", "disadvantages")}),
     )
-    ordering = ('group', 'subgroup_name')
+    ordering = ("group", "subgroup_name")
+
 
 @admin.register(Advantage)
 class AdvantageAdmin(admin.ModelAdmin):
-    list_display = ('description',)
-    search_fields = ('description',)
-    list_filter = ('description',)
-    ordering = ('description',)
+    list_display = ("description",)
+    search_fields = ("description",)
+    list_filter = ("description",)
+    ordering = ("description",)
     fieldsets = (
-        (None, {
-            'fields': ('description',),
-            'description': 'Zadejte popis výhody'
-        }),
+        (None, {"fields": ("description",), "description": "Zadejte popis výhody"}),
     )
-    verbose_name = 'Výhoda'
-    verbose_name_plural = 'Výhody'
+    verbose_name = "Výhoda"
+    verbose_name_plural = "Výhody"
+
 
 @admin.register(Disadvantage)
 class DisadvantageAdmin(admin.ModelAdmin):
-    list_display = ('description',)
-    search_fields = ('description',)
-    list_filter = ('description',)
-    ordering = ('description',)
+    list_display = ("description",)
+    search_fields = ("description",)
+    list_filter = ("description",)
+    ordering = ("description",)
     fieldsets = (
-        (None, {
-            'fields': ('description',),
-            'description': 'Zadejte popis nevýhody'
-        }),
+        (None, {"fields": ("description",), "description": "Zadejte popis nevýhody"}),
     )
-    verbose_name = 'Nevýhoda'
-    verbose_name_plural = 'Nevýhody'
+    verbose_name = "Nevýhoda"
+    verbose_name_plural = "Nevýhody"
--- klimaopatreni/dbklima/models.py
+++ klimaopatreni/dbklima/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
+
 class Localization(models.Model):
-    localization_name = models.CharField(max_length=30, verbose_name="Pojmenování lokalizace")
+    localization_name = models.CharField(
+        max_length=30, verbose_name="Pojmenování lokalizace"
+    )
 
     def __str__(self):
         return self.localization_name
@@ -10,57 +13,69 @@ class Localization(models.Model):
         verbose_name = "Lokalizace"
         verbose_name_plural = "Lokalizace"
 
+
 class Group(models.Model):
-    localization = models.ForeignKey(Localization, on_delete=models.CASCADE, verbose_name="Lokalizace")
+    localization = models.ForeignKey(
+        Localization, on_delete=models.CASCADE, verbose_name="Lokalizace"
+    )
     group_name = models.CharField(max_length=30, verbose_name="Pojmenování skupiny")
 
     def __str__(self):
-        return f'{self.localization}: {self.group_name}'
+        return f"{self.localization}: {self.group_name}"
 
     class Meta:
-        unique_together = ('localization', 'group_name')
+        unique_together = ("localization", "group_name")
         verbose_name = "Skupina"
         verbose_name_plural = "Skupiny"
+
 
 class Advantage(models.Model):
     description = models.CharField(
         max_length=255,
-        verbose_name='Výhoda',
+        verbose_name="Výhoda",
     )
 
     def __str__(self):
         return self.description
 
     class Meta:
-        verbose_name = 'Výhoda'
-        verbose_name_plural = 'Výhody'
+        verbose_name = "Výhoda"
+        verbose_name_plural = "Výhody"
+
 
 class Disadvantage(models.Model):
     description = models.CharField(
         max_length=255,
-        verbose_name='Nevýhoda',
+        verbose_name="Nevýhoda",
     )
 
     def __str__(self):
         return self.description
 
     class Meta:
-        verbose_name = 'Nevýhoda'
-        verbose_name_plural = 'Nevýhody'
+        verbose_name = "Nevýhoda"
+        verbose_name_plural = "Nevýhody"
+
 
 class SubGroup(models.Model):
     group = models.ForeignKey(Group, on_delete=models.CASCADE, verbose_name="Skupina")
-    subgroup_name = models.CharField(max_length=100, verbose_name="Pojmenování podskupiny")
+    subgroup_name = models.CharField(
+        max_length=100, verbose_name="Pojmenování podskupiny"
+    )
     code = models.CharField(max_length=10, verbose_name="Kód opatření", unique=True)
     abstract = models.CharField(max_length=255, verbose_name="Shrnutí opatření")
-    description = models.TextField(verbose_name="Podrobný popis opatření", blank=False, null=False,)
+    description = models.TextField(
+        verbose_name="Podrobný popis opatření",
+        blank=False,
+        null=False,
+    )
     advantages = models.ManyToManyField(Advantage, verbose_name="Výhody")
     disadvantages = models.ManyToManyField(Disadvantage, verbose_name="Nevýhody")
 
     def __str__(self):
-        return f'{self.group}: {self.subgroup_name, self.code}'
+        return f"{self.group}: {self.subgroup_name, self.code}"
 
     class Meta:
-        unique_together = ('group', 'subgroup_name')
+        unique_together = ("group", "subgroup_name")
         verbose_name = "Podskupina"
         verbose_name_plural = "Podskupiny"
